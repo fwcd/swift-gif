3a4c3726c12a202e6762524515c8efdc87cabb29
fwcd <fwcdmail@gmail.com>
2020-09-29 22:28:30 +0200

    Add target dependencies

--- Package.swift
+++ Package.swift
@@ -22,7 +22,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GIFCoder",
-            dependencies: []
+            dependencies: [
+                .product(name: "Utils", package: "swift-utils"),
+                .product(name: "Logging", package: "swift-log")
+            ]
         ),
         .testTarget(
             name: "GIFCoderTests",
