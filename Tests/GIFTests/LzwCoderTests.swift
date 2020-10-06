d92a08f484ece3e140c04c0b872f877fd7c2c0cb
fwcd <fwcdmail@gmail.com>
2020-10-06 19:00:15 +0200

    Minor stylistic tweaks to the LZW coder test

--- Tests/GIFTests/LzwCoderTests.swift
+++ Tests/GIFTests/LzwCoderTests.swift
@@ -23,7 +23,7 @@ final class LzwCoderTests: XCTestCase {
 
     func testLzwEncoder() throws {
         var encoder = LzwEncoder(colorCount: 4)
-        var data = BitData()
+        var encoded = BitData()
         var i = 0
 
         // Each code uses 3 bits in the output
@@ -32,41 +32,41 @@ final class LzwCoderTests: XCTestCase {
 
         // See http://giflib.sourceforge.net/whatsinagif/lzw_image_data.html#lzw_bytes
         // for details on this example
-        encodeNext(&encoder, &i, &data)
+        encodeNext(with: &encoder, into: &encoded, &i)
         XCTAssertEqual(encoder.table.meta.count, 6)
-        XCTAssertEqual(data.bytes, [0b00000100]) // #4
+        XCTAssertEqual(encoded.bytes, [0b00000100]) // #4
 
-        encodeNext(&encoder, &i, &data)
+        encodeNext(with: &encoder, into: &encoded, &i)
         XCTAssertEqual(encoder.table.meta.count, 7)
-        XCTAssertEqual(data.bytes, [0b00001100]) // #4 #1
+        XCTAssertEqual(encoded.bytes, [0b00001100]) // #4 #1
 
-        encodeNext(&encoder, &i, &data)
+        encodeNext(with: &encoder, into: &encoded, &i)
         XCTAssertEqual(encoder.table.meta.count, 7)
-        XCTAssertEqual(data.bytes, [0b00001100]) // #4 #1
+        XCTAssertEqual(encoded.bytes, [0b00001100]) // #4 #1
 
-        encodeNext(&encoder, &i, &data)
+        encodeNext(with: &encoder, into: &encoded, &i)
         XCTAssertEqual(encoder.table.meta.count, 8)
-        XCTAssertEqual(data.bytes, [0b10001100, 0b00000001]) // #4 #1 #6
+        XCTAssertEqual(encoded.bytes, [0b10001100, 0b00000001]) // #4 #1 #6
 
-        encodeNext(&encoder, &i, &data)
+        encodeNext(with: &encoder, into: &encoded, &i)
         XCTAssertEqual(encoder.table.meta.count, 8)
-        XCTAssertEqual(data.bytes, [0b10001100, 0b00000001]) // #4 #1 #6
+        XCTAssertEqual(encoded.bytes, [0b10001100, 0b00000001]) // #4 #1 #6
 
-        encodeNext(&encoder, &i, &data)
+        encodeNext(with: &encoder, into: &encoded, &i)
         XCTAssertEqual(encoder.table.meta.count, 9)
         XCTAssertEqual(encoder.table.meta.codeSize, 4)
-        XCTAssertEqual(data.bytes, [0b10001100, 0b00001101]) // #4 #1 #6 #6
+        XCTAssertEqual(encoded.bytes, [0b10001100, 0b00001101]) // #4 #1 #6 #6
 
-        encodeNext(&encoder, &i, &data)
+        encodeNext(with: &encoder, into: &encoded, &i)
         XCTAssertEqual(encoder.table.meta.count, 10)
-        XCTAssertEqual(data.bytes, [0b10001100, 0b00101101, 0b00000000]) // #4 #1 #6 #6 #2
+        XCTAssertEqual(encoded.bytes, [0b10001100, 0b00101101, 0b00000000]) // #4 #1 #6 #6 #2
 
-        encodeNext(&encoder, &i, &data)
+        encodeNext(with: &encoder, into: &encoded, &i)
         XCTAssertEqual(encoder.table.meta.count, 10)
-        XCTAssertEqual(data.bytes, [0b10001100, 0b00101101, 0b00000000]) // #4 #1 #6 #6 #2
+        XCTAssertEqual(encoded.bytes, [0b10001100, 0b00101101, 0b00000000]) // #4 #1 #6 #6 #2
     }
 
-    private func encodeNext(_ encoder: inout LzwEncoder, _ i: inout Int, _ data: inout BitData) {
+    private func encodeNext(with encoder: inout LzwEncoder, into data: inout BitData, _ i: inout Int) {
         encoder.encodeAndAppend(index: indices[i], into: &data)
         i += 1
     }
