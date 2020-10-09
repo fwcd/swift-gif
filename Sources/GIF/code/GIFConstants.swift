5e83e3dfc70ea644d0c09066beafdb2fccb5ffa4
fwcd <fwcdmail@gmail.com>
2020-10-09 17:28:55 +0200

    Update comment on GIFConstants

--- Sources/GIF/code/GIFConstants.swift
+++ Sources/GIF/code/GIFConstants.swift
@@ -1,7 +1,9 @@
 struct GIFConstants {
+    // Standard color configuration, mainly for encoding
+
     static let colorCount = 256
     static let colorChannels = 3
-    static let colorResolution: UInt8 = 0b111 // A standard value, between 0 and 8 (exclusive) -> Will be interpreted as (bits per pixel - 1)
+    static let colorResolution: UInt8 = 0b111 // between 0 and 8 (exclusive) -> Will be interpreted as (bits per pixel - 1)
     static let backgroundColorIndex: UInt8 = 0xFF
     static let nonTransparentColorCount = colorCount - 1
 
