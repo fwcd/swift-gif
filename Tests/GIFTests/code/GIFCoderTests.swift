dc0d0af0ffea17c989be9328ddfbeb756cc6e1e8
fwcd <fwcdmail@gmail.com>
2020-10-10 20:59:19 +0200

    Add smaller sample GIF

--- Tests/GIFTests/code/GIFCoderTests.swift
+++ Tests/GIFTests/code/GIFCoderTests.swift
@@ -1,5 +1,8 @@
 import XCTest
+import Logging
 @testable import GIF
+
+fileprivate let log = Logger(label: "GIFTests.GIFCoderTests")
 
 final class GIFCoderTests: XCTestCase {
     static var allTests = [
@@ -7,7 +10,9 @@ final class GIFCoderTests: XCTestCase {
     ]
 
     func testGIFCoder() throws {
-        for resource in ["mandelbrot"] {
+        for resource in ["mini", "mandelbrot"] {
+            log.info("Testing GIF en-/decoder with \(resource).gif...")
+
             let url = Bundle.module.url(forResource: resource, withExtension: "gif")!
             let data = try Data(contentsOf: url)
             let gif = try GIF(data: data) // Try decoding the GIF
