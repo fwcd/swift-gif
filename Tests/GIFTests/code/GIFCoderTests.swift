449b8290fc53fa6e4a49f624dbde0510342b584a
fwcd <fwcdmail@gmail.com>
2020-10-10 21:06:57 +0200

    Perform re-decoding in unit test

--- Tests/GIFTests/code/GIFCoderTests.swift
+++ Tests/GIFTests/code/GIFCoderTests.swift
@@ -1,5 +1,6 @@
 import XCTest
 import Logging
+import Graphics
 @testable import GIF
 
 fileprivate let log = Logger(label: "GIFTests.GIFCoderTests")
@@ -17,8 +18,23 @@ final class GIFCoderTests: XCTestCase {
             let data = try Data(contentsOf: url)
             let gif = try GIF(data: data) // Try decoding the GIF
             let reEncoded = try gif.encoded() // Try encoding it again
+            let reDecoded = try GIF(data: reEncoded) // Try decoding it again
 
-            XCTAssertEqual(data, reEncoded)
+            for (frame1, frame2) in zip(gif.frames, reDecoded.frames) {
+                assertImagesEqual(frame1.image, frame2.image)
+                XCTAssertEqual(frame1.delayTime, frame2.delayTime)
+            }
+        }
+    }
+
+    private func assertImagesEqual(_ image1: Image, _ image2: Image) {
+        XCTAssertEqual(image1.width, image2.width)
+        XCTAssertEqual(image1.height, image2.height)
+
+        for y in 0..<image1.height {
+            for x in 0..<image1.width {
+                XCTAssertEqual(image1[y, x], image2[y, x])
+            }
         }
     }
 }
