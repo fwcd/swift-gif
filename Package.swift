a82272eb8a4003e30331756eab5c37c257c07fd5
fwcd <fwcdmail@gmail.com>
2023-02-20 02:01:51 +0100

    Bump swift-utils and swift-graphics

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
-        .package(url: "https://github.com/fwcd/swift-utils.git", from: "1.1.0"),
-        .package(url: "https://github.com/fwcd/swift-graphics.git", from: "2.0.1"),
+        .package(url: "https://github.com/fwcd/swift-utils.git", from: "2.0.0"),
+        .package(url: "https://github.com/fwcd/swift-graphics.git", from: "2.1.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.3.1"))
     ],
     targets: [
